60667adb4f7a92cdb5c1b4ae32ea7bfd0034ad05
batonych <batonych@gmail.com>
2018-11-29 00:33:40 +0300

    implement creating Redis client

---
+++ redis/redis.go
@@ -0,0 +1,28 @@
+package redis
+
+import (
+	"gopkg.in/redis.v3"
+)
+
+type Config struct {
+	Endpoint string `json:"endpoint"`
+	Password string `json:"password"`
+	Database int64  `json:"database"`
+	PoolSize int    `json:"poolSize"`
+}
+
+type Client struct {
+	client *redis.Client
+}
+
+func NewRedisClient(cfg *Config) *Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     cfg.Endpoint,
+		Password: cfg.Password,
+		DB:       cfg.Database,
+		PoolSize: cfg.PoolSize,
+	})
+	return &Client{
+		client: client,
+	}
+}
